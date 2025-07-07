9c875bbaf9c97f5acaf02f8e388bc342759f2219
matanhayon <matanhayon@gmail.com>
2025-07-07 16:23:48 +0300

    working upload photo to recipe

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -2,6 +2,7 @@ import { v4 as uuidv4 } from "uuid";
 import { getSupabaseClient } from "../services/supabaseClient.js";
 
 const BUCKET_NAME = "brewery-images";
+const RECIPE_BUCKET = "recipe-images";
 
 export async function uploadBreweryPhoto(req, res) {
   const file = req.file;
@@ -36,6 +37,51 @@ export async function uploadBreweryPhoto(req, res) {
     }
 
     const { data } = supabase.storage.from(BUCKET_NAME).getPublicUrl(filePath);
+    console.log("DATA " + data);
+
+    return res.status(200).json({ publicUrl: data.publicUrl });
+  } catch (err) {
+    console.error("Upload error:", err);
+    return res.status(500).json({ error: "Unexpected error" });
+  }
+}
+
+export async function uploadRecipePhoto(req, res) {
+  const file = req.file;
+  const authHeader = req.headers.authorization || "";
+  const token = authHeader.replace("Bearer ", "");
+
+  const userId = req.body.user_id;
+  console.log("user ID: " + userId);
+
+  if (!file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+
+  if (!userId) {
+    return res.status(400).json({ error: "Missing user_id" });
+  }
+
+  try {
+    const supabase = getSupabaseClient(token);
+
+    const fileExt = file.originalname.split(".").pop();
+    const uniqueFileName = `${uuidv4()}.${fileExt}`;
+    const filePath = `${userId}/${uniqueFileName}`;
+
+    const { error: uploadError } = await supabase.storage
+      .from(RECIPE_BUCKET)
+      .upload(filePath, file.buffer);
+
+    if (uploadError) {
+      console.error("Supabase upload error:", uploadError.message);
+      return res.status(500).json({ error: "Failed to upload file" });
+    }
+
+    const { data } = supabase.storage
+      .from(RECIPE_BUCKET)
+      .getPublicUrl(filePath);
+    console.log("DATA " + data);
 
     return res.status(200).json({ publicUrl: data.publicUrl });
   } catch (err) {
--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,11 +1,16 @@
 import express from "express";
 import multer from "multer";
-import { uploadBreweryPhoto } from "../controllers/uploadController.js";
+import {
+  uploadBreweryPhoto,
+  uploadRecipePhoto,
+} from "../controllers/uploadController.js";
 
 const router = express.Router();
 const upload = multer(); // Use memory storage by default
 
 // Route: POST /upload/brewery-photo
 router.post("/brewery-photo", upload.single("file"), uploadBreweryPhoto);
+
+router.post("/recipe-photo", upload.single("file"), uploadRecipePhoto);
 
 export default router;
